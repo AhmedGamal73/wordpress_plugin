e00942bda9cb492070a8c5f990038206ed801228
AhmedGamal73 <malokokha2@gmail.com>
2023-03-29 17:51:16 +0200

    Make the custom style function and css file

--- first_plugin/index.php
+++ first_plugin/index.php
@@ -1,6 +1,15 @@
 <?php
 /*
  * Plugin Name: First Plugin
- * Author: Ahmed Gamal
+ * Author: Mo Gamal
  *
  * */
+
+add_action('wp_head', 'add_custom_style');
+
+
+function add_custom_style() {
+  $css_url = plugins_url('css/style.css', __file__);
+  echo "<link rel='stylesheet' href='{$css_url}'>";
+};
+
