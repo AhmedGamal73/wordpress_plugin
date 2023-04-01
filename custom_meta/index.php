a2d7d6918ff5ae7d9b69e222bfa8d26dda7d5713
AhmedGamal73 <malokokha2@gmail.com>
2023-04-01 17:39:51 +0200

    Add custom meta plugin

--- custom_meta/index.php
+++ custom_meta/index.php
@@ -1,16 +1,15 @@
 <?php
 
-
 /*
- * Plugin Name: Custom Meta
- * Description: Adding custom meta tag
- * Author: Ahmed Gamal
+ * Plugin Name: Add Custom Meta
+ * Description: Adding custom meta tag content to the index.html head tag
+ * Author: Ampliz
  */
 
 
 add_action('wp_head', 'custom_meta');
 
 function custom_meta () {
-  $meta_content = 'conten of the meta tag';
-  echo " <meta name='Description' content='{$meta_content}'";
+  $custom_meta = 'custom content';
+  echo "<meta name='Description' content='{$custom_meta}'";
 };
